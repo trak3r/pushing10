7559c10c9b9779194b0c964308edba0f30e1ac91
Thomas "Teflon Ted" Davis <github@rudiment.net>
2026-06-12 09:41:50 -0400

    Fix favicon: add shortcut icon link and /favicon.ico redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "game#dashboard"
 
+  get "favicon.ico", to: redirect("/icon.svg")
+
   post "fly", to: "game#do_fly"
   post "passengers/:id/board", to: "game#board", as: :board_passenger
   post "passengers/:id/unboard", to: "game#unboard", as: :unboard_passenger
